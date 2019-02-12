83621699626ee2fbd4857deb7740549ddb9029f7
Attapong <attapong@attapongs-macbook-pro.local>
2019-02-12 16:02:45 +0700

    Initial Commit

---
+++ NotificationCenter/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  NotificationCenter
+//
+//  Created by Attapong on 12/2/2562 BE.
+//  Copyright © 2562 Attapong. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ NotificationCenter/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  NotificationCenter
+//
+//  Created by Attapong on 12/2/2562 BE.
+//  Copyright © 2562 Attapong. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
